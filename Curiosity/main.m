de680fe161a774eafdf3e717db0ebd6b2372c069
vincent <vincent.coetzee@icloud.com>
2017-06-04 01:46:14 +0200

    Initial Commit

---
+++ Curiosity/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Curiosity
+//
+//  Created by Vincent Coetzee on 2017/06/04.
+//  Copyright © 2017 Vincent Coetzee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
